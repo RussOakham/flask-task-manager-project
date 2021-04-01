fe129db959f8f69841b98318dcbfb4a569693243
RussOakham <rjoakham@gmail.com>
2021-04-01 10:53:59 +0000

    Task Collapsible setup

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,9 @@
+// Mobile Sidebar
 $(document).ready(function(){
     $('.sidenav').sidenav({edge: "right"});
 });
+
+// Accordian Task View
+$(document).ready(function(){
+    $('.collapsible').collapsible();
+  });
